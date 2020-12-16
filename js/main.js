665f6720e8929eb9b96d60694ec946298f0fba32
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-16 20:37:42 +0000

    Create a API request for the value in the search input. #2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 
 var symbol = "";
+var apiKey = 'JI3EUIMS58M4XZ08';
+
+var requestReturn;
 
 document.addEventListener('submit', function (e) {
   e.preventDefault();
   symbol = document.forms["search-symbol-form"].elements["symbol-search"].value.toUpperCase();
 
-  // console.log(symbol);
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    'GET',
+    'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + symbol + '&apikey=' + apiKey);
+  xhr.responseType = 'json';
+
+  xhr.addEventListener('load', function (e) {
+    requestReturn = xhr.response;
+  })
+
+  xhr.send();
 });
