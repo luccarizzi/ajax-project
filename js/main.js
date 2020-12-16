9ed44873415af18fd40e405b267d9069242791a3
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-16 20:01:43 +0000

    Store the value of the search input in a variable. #2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 
+var symbol = "";
+
 document.addEventListener('submit', function (e) {
   e.preventDefault();
+  symbol = document.forms["search-symbol-form"].elements["symbol-search"].value.toUpperCase();
+
+  // console.log(symbol);
 });
